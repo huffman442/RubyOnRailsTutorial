ce2969cda18f15001b86a2a552866e0349063be8
Michael Huffman <huffman442@yahoo.com>
2018-10-01 11:45:16 -0400

    Added edit function to articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,6 +10,10 @@ class ArticlesController < ApplicationController
     def new
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     def create
         @article = Article.new(article_params)
     
